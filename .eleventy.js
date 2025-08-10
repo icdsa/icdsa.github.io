4f192bef8d3908b7cd0d141efe5db1434f942bd4
Will Denny <will@wjdenny.com>
2025-08-09 20:01:22 -0500

    use eleventy-img to optimize images

--- .eleventy.js
+++ .eleventy.js
@@ -1,3 +1,5 @@
+const { filenameFormat } = require("@11ty/eleventy-img/src/image-path");
+
 function configureMarkdownIt() {
   // reference: https://github.com/markdown-it/markdown-it-container/issues/23
 
@@ -18,6 +20,26 @@ function configureMarkdownIt() {
 
 // This block defines the configuration settings for 11ty
 module.exports = function (eleventyConfig) {
+
+    eleventyConfig.addPlugin(require("@11ty/eleventy-img").eleventyImageTransformPlugin, {
+        // output image formats
+        formats: ["avif", "png", "webp", "jpeg"],
+
+        // output image widths
+        widths: [ 320, 760, 1500],
+
+        htmlOptions: {
+            imgAttributes: {
+                loading: "lazy",
+                decoding: "async"
+            },
+            pictureAttributes: {}
+        },
+        hashLength: 16,
+        filenameFormat: function (id, src, width, format, options) {
+            return `${id}-${width}.${format}`
+        }
+    });
 
     // Set the markdown configuration in 11ty
     eleventyConfig.setLibrary("md", configureMarkdownIt());
