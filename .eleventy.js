464a8110838ccd12954d614aa4216deb943deb55
Will Denny <will@wjdenny.com>
2025-08-09 19:08:19 -0500

    use markdown-it for attrs and container

--- .eleventy.js
+++ .eleventy.js
@@ -1,5 +1,26 @@
+function configureMarkdownIt() {
+  // reference: https://github.com/markdown-it/markdown-it-container/issues/23
+
+  return require("markdown-it")({ html: true })
+    .use(require('markdown-it-attrs'))
+    .use(require('markdown-it-container'), 'dynamic', {
+      validate: function () { return true; },
+      render: function (tokens, idx) {
+        const token = tokens[idx];
+        if (token.nesting === 1) {
+          return '<div class="' + token.info.trim() + '">';
+        } else {
+          return '</div>';
+        }
+      }
+    });
+}
+
 // This block defines the configuration settings for 11ty
 module.exports = function (eleventyConfig) {
+
+    // Set the markdown configuration in 11ty
+    eleventyConfig.setLibrary("md", configureMarkdownIt());
 
     // Recursively copy over all files from src/assets to public/assets
     eleventyConfig.addPassthroughCopy("src/assets");
